9bf0fda085e2ec6952999c7fb4218129e777a34c
asrani <asrani.27@gmail.com>
2021-01-20 10:00:22 +0800

    galery

---
+++ resources/views/galery.blade.php
@@ -0,0 +1,19 @@
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card card-secondary">
+        <div class="card-header">
+            <h5 class="card-title m-0"> <i class="fas fa-file"></i> Galery Kegiatan </h5>
+        </div>
+        <div class="card-body table-responsive">
+            <div class="timeline-body">
+                <img src="http://placehold.it/150x100" alt="...">
+                <img src="http://placehold.it/150x100" alt="...">
+                <img src="http://placehold.it/150x100" alt="...">
+                <img src="http://placehold.it/150x100" alt="...">
+                <img src="http://placehold.it/150x100" alt="...">
+              </div>
+        </div>
+        </div>
+    </div>
+    <!-- /.col-md-6 -->
+</div>
