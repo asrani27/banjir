287bdb6552047ae4013caaf2bd712ffaed2219c6
asrani <asrani.27@gmail.com>
2021-01-20 20:50:33 +0800

    link gambar target new tab

--- resources/views/galery.blade.php
+++ resources/views/galery.blade.php
@@ -3,7 +3,7 @@
 
 <div class="swiper-slide">
 <div class="image-container">
-<img class="img-responsive" src="/storage/{{$item->file}}" width="180px" height="160px" alt="alternative">
+<a href="/storage/{{$item->file}}" target="_blank"><img class="img-responsive" src="/storage/{{$item->file}}" width="180px" height="160px" alt="alternative"></a>
 </div>
 </div>
 @endforeach
