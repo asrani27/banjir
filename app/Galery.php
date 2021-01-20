ce0072f28d6b16366a14aa15c01895298563cbe4
asrani <asrani.27@gmail.com>
2021-01-20 10:12:43 +0800

    galery

--- app/Galery.php
+++ app/Galery.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Galery extends Model
 {
-    //
+    protected $table = 'galery';
+
+    protected $guarded = ['id'];
+
+    public $timestamps = false;
 }
