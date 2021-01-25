d31c78d080297f2830672c13960276ca3ddb90d4
asrani <asrani.27@gmail.com>
2021-01-25 08:56:40 +0800

    test

--- app/Http/Controllers/DataJsonController.php
+++ app/Http/Controllers/DataJsonController.php
@@ -57,8 +57,8 @@ class DataJsonController extends Controller
     {
         $now = Carbon::now()->format('dmYHi');
         $data = collect(json_decode(DataJson::find($id)->json_rekap));
-        $tanggal = DataJson::find($id)->tanggal;
-        
+        $tanggal = Carbon::parse(DataJson::find($id)->tanggal)->isoFormat('d MMMM Y');
+        dd($tanggal);
         $pdf = PDF::loadView('admin.pdf.rekap', compact('data','tanggal'));
         return $pdf->download('rekapitulasi'.$now.'.pdf');
     }
